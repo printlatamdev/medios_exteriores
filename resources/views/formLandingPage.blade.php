6b1d706dafa130cac86625e828c4977e505ef60d
unknown <admin@IT1.colordigital.local>
2024-10-25 14:59:59 -0600

    Quit images

--- resources/views/formLandingPage.blade.php
+++ resources/views/formLandingPage.blade.php
@@ -4,12 +4,6 @@
 <head>
 
 	<title>DashboardKit, @yield('title')</title>
-	<!-- HTML5 Shim and Respond.js IE11 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 11]>
-		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-		<![endif]-->
 	<!-- Meta -->
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
@@ -21,6 +15,7 @@
 
 	<!-- Favicon icon -->
 	<link rel="icon" href="assets/images/favicon.svg" type="image/x-icon">
+  <link rel="stylesheet" href="{{ asset('vendorSystemFile/css/app.css') }}">
 
 	<!-- font css -->
 	<!-- <link rel="stylesheet" href="{{  asset('assets/fonts/feather.css')     }}">
@@ -68,12 +63,6 @@
           border: 0px !important; */
           background-color: #045aa0 !important;
         }
-        .logo-image-navbar{
-           width: 200px; 
-           height: 35px; 
-           margin: 0; 
-           padding:0;
-        }
         .color-letra-menu{
           color: white;
         }
@@ -89,7 +78,7 @@
           border-radius: 3px 3px 3px 3px;
           behavior:url(border.htc);
 
-          box-shadow: 0px 0px 12px #000000;
+          /*box-shadow: 0px 0px 2px #000000; */
           -ms-filter: 'progid:DXImageTransform.Microsoft.Shadow(Strength=12, Direction=135, Color=#000000)'; 
           filter: progid:DXImageTransform.Microsoft.Shadow(Strength=12, Direction=135, Color='#000000');
           
@@ -125,7 +114,7 @@
     <nav class="logo-imagen navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
-             <img class="logo-image-navbar" src="{{  asset('vendorSystemFile/image/logo.png') }}" class="img-fluid" >
+             <img class="logo-image-navbar" src="{{  asset('vendorSystemFile/image/logo.png') }}">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
--- resources/views/vistasLogin/formLoginUsuario.blade.php
+++ resources/views/vistasLogin/formLoginUsuario.blade.php
@@ -26,14 +26,14 @@
     			<div class="card-body">
 
     				<!-- <img src="assets/images/logo-dark.svg" alt="" class="img-fluid mb-4"> -->
-    				<h4 class="mb-3 align-items-center text-center">Signin</h4>
+    				<h4 class="mb-3 align-items-center text-center">Iniciar sesión</h4>
     				<div class="input-group mb-3">
     					<span class="input-group-text"><i class="fa-solid fa-envelope-circle-check"></i></span>
-    					<input type="email" id="email" class="form-control" placeholder="Email address">
+    					<input type="email" id="email" class="form-control" placeholder="Correo electrónico">
     				</div>
     				<div class="input-group mb-4">
     					<span class="input-group-text"><i class="fa-solid fa-lock"></i></span>
-    					<input type="password" id="password" class="form-control" placeholder="Password">
+    					<input type="password" id="password" class="form-control" placeholder="Contraseña">
     				</div>
     
     				<input type="hidden" name="_token" id="token" value="{{ csrf_token() }}">
@@ -41,13 +41,13 @@
     				<div class="form-group text-left mt-2">
                             <div class="form-check form-switch">
                                 <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
-                                <label class="form-check-label" for="flexSwitchCheckChecked">Save credentials</label>
+                                <label class="form-check-label" for="flexSwitchCheckChecked">Guardar credenciales</label>
                             </div>
     				</div>
 
 					<div class="form-group text-center">
     				    <button class="btn btn-block btn-primary mb-4" id="btnIniciarSesion" onclick="formLuApp.btnIniciarSesion();"><i class="fa-solid fa-power-off"></i> Inicial sesion</button>
-    				    <p class="mb-0 text-muted">Don’t have an account? <a href="{{route('registro.index')}}" class="f-w-400">Signup</a></p>
+    				    <!--<p class="mb-0 text-muted">¿Aún no tienes cuenta? <a href="{{route('registro.index')}}" class="f-w-400">Crear cuenta</a></p>-->
 					</div>
 				</div>
     		<!-- </div> -->
