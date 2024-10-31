d98f2ef22f4d90d3c75c4d7d30141d5a60cca3f9
unknown <admin@IT1.colordigital.local>
2024-10-31 10:09:51 -0600

    Change inputs in budget form

--- app/Filament/Resources/BudgetResource.php
+++ app/Filament/Resources/BudgetResource.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\BudgetResource\Pages;
 use App\Models\Budget;
+use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
@@ -12,6 +13,7 @@ use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Pelmered\FilamentMoneyField\Forms\Components\MoneyInput;
 
 class BudgetResource extends Resource
 {
@@ -28,28 +30,28 @@ class BudgetResource extends Resource
         return $form
             ->schema([
                 Section::make('Fianza')->schema([
-                    TextInput::make('expiration_date_bail')->label('Fecha de vencimiento'),
-                    TextInput::make('payment_date_bail')->label('Fecha de pago'),
-                    TextInput::make('total_bail')->label('Total'),
+                    DatePicker::make('expiration_date_bail')->label('Fecha de vencimiento'),
+                    DatePicker::make('payment_date_bail')->label('Fecha de pago'),
+                    MoneyInput::make('total_bail')->label('Total'),
                 ])->columns(3),
                 Section::make('Seguro de daños a terceros')->schema([
-                    TextInput::make('expiration_date_damageinsurance')->label('Fecha de vencimiento'),
-                    TextInput::make('payment_date_damageinsurance')->label('Fecha de pago'),
-                    TextInput::make('total_damageinsurance')->label('Total'),
+                    DatePicker::make('expiration_date_damageinsurance')->label('Fecha de vencimiento'),
+                    DatePicker::make('payment_date_damageinsurance')->label('Fecha de pago'),
+                    MoneyInput::make('total_damageinsurance')->label('Total'),
                 ])->columns(3),
                 Section::make('Alcaldía')->schema([
-                    TextInput::make('expiration_date_municipality')->label('Fecha de vencimiento'),
-                    TextInput::make('payment_date_municipality')->label('Fecha de pago'),
-                    TextInput::make('total_municipality')->label('Total'),
+                    DatePicker::make('expiration_date_municipality')->label('Fecha de vencimiento'),
+                    DatePicker::make('payment_date_municipality')->label('Fecha de pago'),
+                    MoneyInput::make('total_municipality')->label('Total'),
                 ])->columns(3),
                 Section::make('Arrendamiento')->schema([
-                    TextInput::make('total_rental')->label('Fecha de vencimiento'),
-                    TextInput::make('payment_date_municipality')->label('Fecha de pago'),
-                    TextInput::make('total_rental')->label('Total'),
+                    DatePicker::make('expiration_date_rental')->label('Fecha de vencimiento'),
+                    DatePicker::make('payment_date_rental')->label('Fecha de pago'),
+                    MoneyInput::make('total_rental')->label('Total'),
                 ])->columns(3),
                 Section::make('Mantenimiento')->schema([
                     Textarea::make('maintenance_description')->label('Descripción de mantenimiento')->columnSpan(2),
-                    TextInput::make('total_maintenance')->label('Fecha de vencimiento'),
+                    MoneyInput::make('total_maintenance')->label('Fecha de vencimiento'),
                 ])->columns(3),
                 Section::make('Energía eléctrica')->schema([
                     Select::make('electricity_provider')
@@ -63,9 +65,9 @@ class BudgetResource extends Resource
                         ->required(),
                     TextInput::make('electricity_NIC_NIS')->label('NIC/NIS'),
                     TextInput::make('electricity_new_NC')->label('Nuevo NC'),
-                    TextInput::make('expiration_date_electricity')->label('Fecha de vencimiento'),
-                    TextInput::make('payment_date_electricity')->label('Fecha de pago'),
-                    TextInput::make('total_electricity')->label('Total'),
+                    DatePicker::make('expiration_date_electricity')->label('Fecha de vencimiento'),
+                    DatePicker::make('payment_date_electricity')->label('Fecha de pago'),
+                    MoneyInput::make('total_electricity')->label('Total'),
                 ])->columns(4),
             ]);
     }
--- app/Models/Budget.php
+++ app/Models/Budget.php
@@ -7,27 +7,33 @@ use Illuminate\Database\Eloquent\Model;
 class Budget extends Model
 {
     protected $fillable = [
-        'total_bail',
         'expiration_date_bail',
         'payment_date_bail',
-        'total_damageinsurance',
+        'total_bail',
+        
         'expiration_date_damageinsurance',
         'payment_date_damageinsurance',
-        'total_municipality',
+        'total_damageinsurance',
+
         'expiration_date_municipality',
         'payment_date_municipality',
-        'total_rental',
+        'total_municipality',
+
         'expiration_date_rental',
         'payment_date_rental',
-        'total_maintenance',
+        'total_rental',
+
         'maintenance_description',
+        'total_maintenance',        
+
         'illuminated',
         'electricity_provider',
         'electricity_NIC_NIS',
         'electricity_new_NC',
-        'total_electricity',
         'expiration_date_electricity',
         'payment_date_electricity',
+        'total_electricity',
+
         'total_payment',
     ];
 
