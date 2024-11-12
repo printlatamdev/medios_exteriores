9ca08bd30df8e91140d75d3a4d8561e50ece1052
unknown <admin@IT1.colordigital.local>
2024-11-12 14:11:05 -0600

    Added gate in service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Gate::policy(\App\Models\User::class, \App\Policies\UserPolicy::class);
+        Gate::policy(\App\Models\Budget::class, \App\Policies\BudgetPolicy::class);
+        Gate::policy(\App\Models\Externalmedia::class, \App\Policies\ExternalmediaPolicy::class);
+        Gate::policy(\App\Models\Role::class, \App\Policies\RolePolicy::class);
     }
 }
+
--- config/filament-shield.php
+++ config/filament-shield.php
@@ -3,7 +3,7 @@
 return [
     'shield_resource' => [
         'should_register_navigation' => true,
-        'slug' => 'roles',
+        'slug' => 'shield/roles',
         'navigation_sort' => -1,
         'navigation_badge' => true,
         'navigation_group' => true,
@@ -12,6 +12,8 @@ return [
         'is_scoped_to_tenant' => true,
         'cluster' => null,
     ],
+
+    'tenant_model' => null,
 
     'auth_provider_model' => [
         'fqcn' => 'App\\Models\\User',
