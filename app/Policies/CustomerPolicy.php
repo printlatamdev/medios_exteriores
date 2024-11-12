9387989c9c85d6858fff70e27bd0368a556111d8
unknown <admin@IT1.colordigital.local>
2024-11-12 15:48:59 -0600

    Publish policies

--- app/Policies/CustomerPolicy.php
+++ app/Policies/CustomerPolicy.php
@@ -3,10 +3,10 @@
 namespace App\Policies;
 
 use App\Models\User;
-use App\Models\Externalmedia;
+use App\Models\Customer;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
-class ExternalmediaPolicy
+class CustomerPolicy
 {
     use HandlesAuthorization;
 
@@ -15,15 +15,15 @@ class ExternalmediaPolicy
      */
     public function viewAny(User $user): bool
     {
-        return $user->can('view_any_externalmedia');
+        return $user->can('view_any_customer');
     }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Externalmedia $externalmedia): bool
+    public function view(User $user, Customer $customer): bool
     {
-        return $user->can('view_externalmedia');
+        return $user->can('view_customer');
     }
 
     /**
@@ -31,23 +31,23 @@ class ExternalmediaPolicy
      */
     public function create(User $user): bool
     {
-        return $user->can('create_externalmedia');
+        return $user->can('create_customer');
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Externalmedia $externalmedia): bool
+    public function update(User $user, Customer $customer): bool
     {
-        return $user->can('update_externalmedia');
+        return $user->can('update_customer');
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Externalmedia $externalmedia): bool
+    public function delete(User $user, Customer $customer): bool
     {
-        return $user->can('delete_externalmedia');
+        return $user->can('delete_customer');
     }
 
     /**
@@ -55,15 +55,15 @@ class ExternalmediaPolicy
      */
     public function deleteAny(User $user): bool
     {
-        return $user->can('delete_any_externalmedia');
+        return $user->can('delete_any_customer');
     }
 
     /**
      * Determine whether the user can permanently delete.
      */
-    public function forceDelete(User $user, Externalmedia $externalmedia): bool
+    public function forceDelete(User $user, Customer $customer): bool
     {
-        return $user->can('force_delete_externalmedia');
+        return $user->can('force_delete_customer');
     }
 
     /**
@@ -71,15 +71,15 @@ class ExternalmediaPolicy
      */
     public function forceDeleteAny(User $user): bool
     {
-        return $user->can('force_delete_any_externalmedia');
+        return $user->can('force_delete_any_customer');
     }
 
     /**
      * Determine whether the user can restore.
      */
-    public function restore(User $user, Externalmedia $externalmedia): bool
+    public function restore(User $user, Customer $customer): bool
     {
-        return $user->can('restore_externalmedia');
+        return $user->can('restore_customer');
     }
 
     /**
@@ -87,15 +87,15 @@ class ExternalmediaPolicy
      */
     public function restoreAny(User $user): bool
     {
-        return $user->can('restore_any_externalmedia');
+        return $user->can('restore_any_customer');
     }
 
     /**
      * Determine whether the user can replicate.
      */
-    public function replicate(User $user, Externalmedia $externalmedia): bool
+    public function replicate(User $user, Customer $customer): bool
     {
-        return $user->can('replicate_externalmedia');
+        return $user->can('replicate_customer');
     }
 
     /**
@@ -103,6 +103,6 @@ class ExternalmediaPolicy
      */
     public function reorder(User $user): bool
     {
-        return $user->can('reorder_externalmedia');
+        return $user->can('reorder_customer');
     }
 }
